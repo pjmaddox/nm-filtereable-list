875335a8b511f25ca4110257ac04aa54cc55ea35
pjmaddox24 <pjmaddox24@gmail.com>
2018-07-09 21:36:42 -0500

    initialized app, added necessary tdd packages

---
+++ nm-filterable-list/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  state = {
+    isLoading: true,
+    displayList: []
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ nm-filterable-list/src/App.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+let shallowNode;
+
+beforeEach(() => {
+  shallowNode = shallow(<App />);
+});
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it("should begin in a loading state", () => {
+  expect(shallowNode.state("isLoading")).toEqual(true);
+});
+
+it("should begin with a blank list of items", () =>  {
+  expect(shallowNode.state(displayList)).toEqual([]);
+});
+
+it("should render a list of items", () =>  {
+
+});
+
+it("should begin with a displayed list equal to the empty list", () =>  {
+
+});
